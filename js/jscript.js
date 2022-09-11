dca72c492bc46ebfc8b9a64488c0351ebac2be76
ALichrieff <alichrief12gmail.com>
2022-09-11 09:56:48 +0300

    fix

--- js/jscript.js
+++ js/jscript.js
@@ -16,10 +16,47 @@ window.onload = () => {
 const btn=document.getElementById("btn")
 let userName = ""
 
-function geta() {
+//get the username
+let getUserName = (e) => {
   userName = document.getElementById("user-name").value
- 
+  age=document.getElementById("age")
+  gender=document.getElementById("gender")
+  national=document.getElementById("national")
+
+
+}
+function getGender(){
+  fetch(`https://api.genderize.io?name=${userName}`)
+  .then((response) => response.json())
+  .then((data)=> )
+}
+function getAge(){
+  fetch(`https://api.agify.io/?name=${userName}`)
+  .then((response) => response.json())
+  .then((data)=> show.innerHTML=data.age)
+}
+function getNationality(){
+  fetch(`https://api.nationalize.io/?name=${userName}`)
+  .then((response) => response.json())
+  .then((data)=> {
+    let country= data.country
+    let nat_id = country[0].country_id
+
+    if (country.length==1){
+        nat_id=country[0].country_id
+    }
+    else{
+        nat_id = country[0].country_id + "and his second nationality is " + country[1].country_id
+    }
+  })
 }
 
+
 //define the handlers 
-btn.addEventListener("onclick", geta)
+
+
+btn.addEventListener('click',() => {    
+  getUserName()
+  getAge();
+  getGender();    
+});
