59c16931b7f056729ebbd89650aa0ee0dfe21d21
ALichrieff <alichrief12gmail.com>
2022-09-11 09:15:37 +0300

    laod random dog picture

--- js/jscript.js
+++ js/jscript.js
@@ -1,1 +1,16 @@
+window.onload = () => {
 
+  dog_result=document.querySelector("#dog-results")
+
+  function getRandomDog(){
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response)=>response.json())
+    .then((data)=>{
+          dog_result.innerHTML=`<img src="${data.message}"/>`
+          console.log(data.status)
+
+  })
+  }
+  getRandomDog()
+
+}
